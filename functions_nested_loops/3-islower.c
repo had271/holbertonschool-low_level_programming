88eb237479a6b92a8df73128f8d5601d78846977
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-08-26 15:37:03 +0000

    task.3 commit

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is c lower
+ * Returns 1 if c is lowercase
+ *
+ * Returns 0 otherwise
+ *
+ */
+int _islower(int c)
+{
+	if (c == 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
