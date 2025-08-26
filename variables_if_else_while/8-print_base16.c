4c332853f08c49a3ec752a754d916eb6c0e20a27
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-08-26 12:49:30 +0000

    task.8 commit

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int num;
+
+for (num = 0; num <= 15; num++)
+	putchar(num);
+
+putchar('\n');
+
+return (0);
+}
