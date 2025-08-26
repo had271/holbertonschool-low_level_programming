a55b18647636c8bd74ef1c6bcbb91fc49ff6c3b5
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-08-26 13:21:57 +0000

    task.9 update

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,11 +10,13 @@ int main(void)
 int n;
 
 for (n = '0'; n <= '9'; n++)
-	putchar(n);
-if (n != 9)
 {
-putchar(',');
-putchar(' ');
+	putchar(n);
+	if (n != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
 }
 putchar('\n');
 return (0);
