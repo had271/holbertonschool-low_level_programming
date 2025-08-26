ea6ed955c8ab618ebabbac43df91aa66c6c1b0a1
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-08-26 12:27:53 +0000

    task.7 commit

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z'; c <= 'a'; c--)
+	putchar(c);
+
+putchar('\n');
+
+return (0);
+}
