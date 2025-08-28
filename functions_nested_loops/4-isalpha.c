46de31d005eae7add33791f3da16727494c7881e
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-08-28 10:23:30 +0000

    task.4 commit

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ * _isalpha - Checks for character letter
+ * @c: The character to check
+ *
+ * Return: 1 if c is letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
