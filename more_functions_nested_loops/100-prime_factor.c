98f45d03a8001743f40f3906e21bb4de3b4c0d61
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-09-03 14:15:02 +0000

    task.1.1

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,18 @@
-#include "main.h"
 #include <stdio.h>
 /**
-* primeFactors - A function to print all prime factors
-*@n: given number
-*Return: the prime factors
-*/
-void primeFactors(int n)
+ * main - Prints out the larget prime factor of 612852475143
+ * Return:return 0 with success.
+ */
+int main(void)
 {
-	int i;
+	long i;
+	long num = 612852475143;
 
-	while (n % 2 == 0)
-{
-	printf("%d ", 2);
-	n = n / 2;
-}
-	for (i = 3; i * i <= n; i = i + 2)
-{
-	while (n % i == 0)
-{
-	printf("%d ", i);
-	n = n / i;
-}
-}
-	if (n > 2)
-	printf("%d ", n);
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
 }
