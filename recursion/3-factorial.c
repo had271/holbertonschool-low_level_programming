b3a6d22d051ce5f98e09ea49808f805263fba966
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-09-09 16:02:14 +0000

    add 2-strlen_recursion.c and 3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a given number.
+ * @n: The number to find factorial of.
+ *
+ * Return: return -1 If @n is lower than 0
+ *	   otherwise it will return the factorial of @n
+ */
+int factorial(int n)
+{
+	int result = n;
+
+	if (n < 0)
+		return (-1);
+
+	else if (n >= 0 && n <= 1)
+		return (1);
+
+	result *= factorial(n - 1);
+
+	return (result);
+}
