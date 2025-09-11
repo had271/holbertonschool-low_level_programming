6857b6b43aaa902b8c018db7728d5d633e8dfe4f
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-09-11 14:43:00 +0000

    add task 6-is_prime_number.c

--- recursion/main.h
+++ recursion/main.h
@@ -9,6 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int find_sqrt(int num, int root);
 int is_prime_number(int n);
+int is_divisible(int num, int div);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 #endif
