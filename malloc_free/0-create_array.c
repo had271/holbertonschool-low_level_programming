d375c24c641a4d178d349b0d43ebbf4cd6518882
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-09-16 13:05:04 +0000

    add the 0&1 tasks in malloc_free

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -6,23 +6,21 @@
  * @size: the size of the array
  *@c: the specific char
  *Return: return NULL if the size of the array 0 or filed to return a pointer
- *	Otherwise return pointer	
+ *	Otherwise return pointer
  */
 char *create_array(unsigned int size, char c)
 {
-       	char *array;
+	char *array;
 	unsigned int index;
 
 	if (size == 0)
 		return (NULL);
-
 	array = malloc(sizeof(char) * size);
-	
 	if (array == NULL)
-	       return (NULL);
+		return (NULL);
 
 	for (index = 0; index < size; index++)
-		array[index] =c;
+		array[index] = c;
 
 	return (array);
 }
