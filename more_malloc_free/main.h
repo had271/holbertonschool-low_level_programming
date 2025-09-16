3ad9c15fa4dc1c968042d0169eefac96d62c10d2
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-09-16 14:07:27 +0000

    edit main.h file

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#include "MAIN.H"
-#define "MAIN.H"
+#ifndef MAIN_H
+#define MAIN_H
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
