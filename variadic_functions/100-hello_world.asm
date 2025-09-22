8a00057682bd42caa010b58503e83358c8f4ff81
“had271” <Hadeelmesnad@gmail.com  git config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-09-22 03:32:26 +0000

    add 100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -4,7 +4,7 @@ main:
 	    mov rax, 1
 	    mov rdi, 1
 	    mov rsi, message
-	    mov rdx, 13
+	    mov rdx, message_end - message  ; NASM computes size
 	    syscall
 
 	    mov rax, 60
@@ -12,4 +12,3 @@ main:
 	    syscall
 	  section .data
 message:	 db "Hello, World"
-message_end:
