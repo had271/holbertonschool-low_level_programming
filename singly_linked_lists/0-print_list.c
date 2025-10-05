880b0b8d44a442cba84ee1051fe73e5d0cc2c986
“had271” <Hadeelmesnad@gmail.comgit config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-10-05 13:03:39 +0000

    Fix betty style in Task.0: 0-print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,12 +12,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf ("[0] (nil)\n");
-		else 
-			printf ("[%d] %s\n", h->len, h->str);
-
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		nodes++;
 		h = h->next;
 	}
-	return(nodes);
+	return (nodes);
 }
