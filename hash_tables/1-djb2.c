f444bafab51f98b9ed4e6858f0d681c21f43aaa2
“had271” <Hadeelmesnad@gmail.comgit config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-10-20 15:20:59 +0000

    fix error of betty documentation style

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,3 +1,10 @@
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * *str: string used to generate hash value
+ * Return: hash value
+ */
+
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
@@ -6,7 +13,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	hash = 5381;
 	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + c;
+		hash = ((hash << 5) + hash) + c;/* hash * 33 + c */
 	}
 	return (hash);
 }
